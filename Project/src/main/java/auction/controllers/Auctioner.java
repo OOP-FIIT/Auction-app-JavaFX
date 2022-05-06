aa64279668c06f5c717ccdb26825f263fee0c3dc
Mykhailo Sichkaruk <misha0510@gmail.com>
2022-05-06 14:04:19 +0200

    Auctioner added

--- Project/src/main/java/auction/controllers/Auctioner.java
+++ Project/src/main/java/auction/controllers/Auctioner.java
@@ -1,10 +1,35 @@
 package auction.controllers;
-public class Auctioner extends User{
 
-    //Polymorphism
-    public void handle_button(){
-        //Here will be code for handling auctioner button click
-        //Auctioner could finish auction and calculate winner
-        //And he could stop auction with this button
+import java.sql.SQLException;
+
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+
+public class Auctioner extends User {
+
+    // Polymorphism
+    public void handle_button() {
+        // Here will be code for handling auctioner button click
+        // Auctioner could finish auction and calculate winner
+        // And he could stop auction with this button
+    }
+
+    @Override
+    public void initialize() throws SQLException {
+        add_lot_input.setDisable(true);
+        add_lot_text.setText("Try PRO version to use this)");
+        add_bid_input.setPromptText("You can buy PRO version to use all feachures in one account");
+        add_bid_input.setDisable(true);
+        Vbox_lots.setBackground(new Background(new BackgroundFill(Color.DARKGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+        scroll_lots.setStyle("-fx-background: DARKSLATEGREY; -fx-border-color: #90EE90;");
+        
+        PrintLots();
+        Platform.runLater(() -> add_lot_input.requestFocus());
+        UpdateUserData();
+
     }
 }
