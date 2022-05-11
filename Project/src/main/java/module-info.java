9356bd6910f943a3bfa64a34870561959dfe72f1
Mykhailo Sichkaruk <misha0510@gmail.com>
2022-05-11 19:57:42 +0200

    added import of json and javax.activation

--- Project/src/main/java/module-info.java
+++ Project/src/main/java/module-info.java
@@ -3,7 +3,9 @@ module auction {
     requires javafx.fxml;
     requires java.sql;
     requires javax.mail.api;
+    requires activation;
     requires java.desktop;
+    requires json.simple;
 
     opens auction to javafx.fxml;
     opens auction.controllers to javafx.fxml;
